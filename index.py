efe35051279f63a10d36e3296cb4041f4dced185
darkicecre <vpmhieu.dev@gmail.com>
2025-01-28 23:41:17 +0700

    [Feaute2]

--- index.py
+++ index.py
@@ -1,28 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
 import os
+import json
 
-
-#  python3 -m venv path/to/venv && source path/to/venv/bin/activate
+#  python3 -m venv path/to/venv
+# source path/to/venv/bin/activate
+# python3 -m pip install xyz
+# pip install --upgrade pip
+# pip3 install requests
 #  python3 index.py
-# Đường dẫn đến folder chứa các file txt
-path = "truong-sinh-bat-dau-tu-tap-dich-nuoi-ga/"
 
-# Duyệt qua tất cả các file trong folder
-for x in range(5,39):
-  folder_path = path+str(x)
-  for filename in os.listdir(folder_path):
-      if filename.endswith(".txt"):  # Chỉ xử lý file .txt
-          file_path = os.path.join(folder_path, filename)
-          
-          # Đọc nội dung file
-          with open(file_path, "r", encoding="utf-8") as file:
-              content = file.read()
-          
-          # Replace '\n' thành ký tự xuống dòng thực sự và xóa ký tự '\'
-          content = content.replace(r'\n', '\n\n').replace('\\', '')
+# a=1
+# for i in range(1,100):
+#     print(i)
+#     URL = "https://truyenfull.io/xich-tam-tuan-thien/chuong-"+str(i)+"/"
+#     r = requests.get(url = URL)
+#     html_content=r.text
+#     soup = BeautifulSoup(html_content, "html")
+#     box_chap_div = soup.find("div",{"class":"chapter-c"})
+#     box_chap_tit = soup.find("a",{"class":"chapter-title"})
+#     content = box_chap_tit.get_text(separator="$$$$").strip() + "$$$$" + box_chap_div.get_text(separator="$$$$").strip()
+#     path = "xichtam/"+str(a)+"/"+str(i)+".txt"
+#     os.makedirs(os.path.dirname(path), exist_ok=True)
+#     with open(path, "w") as f:
+#         f.write(content)
+def find_substring_first_occurrence(string, A, B):
+    start_index = string.find(A)
+    if start_index == -1:
+        return None  # Không tìm thấy A
 
-          # Ghi lại nội dung đã chỉnh sửa vào file
-          with open(file_path, "w", encoding="utf-8") as file:
-              file.write(content)
-  print("Hoàn tất xử lý folder "+ str(x))
-
-print("Hoàn tất xử lý các file trong thư mục.")
+    try:
+        end_index = string.index(B, start_index)
+        return string[start_index:end_index + len(B)]
+    except ValueError:
+        return None  # Không tìm thấy B sau A
+    
+m = ""
+for a in range(1,93):
+    URL = "https://truyendocviet.io.vn/doc-truyen/co-duyen-cua-nguoi-rat-tot-ta-vui-long-nhan/read/chapters.html/"+str(a)+".html"
+    r = requests.get(url = URL)
+    html_content=r.text
+    content = find_substring_first_occurrence(html_content,'"arrChapters":',',"arrDocuments')
+    content = content[14:len(content)-14]
+    idx = content.find("CreatedDate")
+    while idx > 0:
+        content = content[0:idx-1] + content[idx+50:len(content)]
+        idx = content.find("CreatedDate")
+    obj = json.loads(content)
+    for index, value in enumerate(obj):
+        print("Quyển " + str(a) + " - chap " + str(index+1))
+        URL = "https://truyendocviet.io.vn/doc-truyen/co-duyen-cua-nguoi-rat-tot-ta-vui-long-nhan/read/"+value['_id']+".html"
+        r = requests.get(url = URL)
+        html_content=r.text
+        ncont = find_substring_first_occurrence(html_content,'"CurrentChapter":','ContentOrder')
+        lcont = find_substring_first_occurrence(ncont,'ContentTitle":',',"Description')
+        lcont = lcont[15:len(lcont)-14]
+        print(lcont)
+        ncont = find_substring_first_occurrence(ncont,'"Description":','ContentOrder')
+        ncont = ncont[15:len(ncont)-15]
+        ncont = ncont.replace(r'\n', '\n\n').replace('\\', '')
+        lcont = lcont.replace(r'\n', '\n\n').replace('\\', '')
+        path = "co-duyen-cua-nguoi-rat-tot-ta-vui-long-nhan/"+str(a)+"/"+str(index+1)+".txt"
+        m = m + str(a) + "_" + str(index+1) + "=" + lcont + "\n"
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "w") as f:
+            f.write(ncont)
+with open("co-duyen-cua-nguoi-rat-tot-ta-vui-long-nhan/mucluc.txt", "w") as f:
+  f.write(m)
+    # print(content)
